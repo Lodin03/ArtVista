c91a87d47c358b6308cfd114137261e5db9fb482
Lars Odin Moen <larsodintveitmoen@Larss-MacBook-Air.local>
2024-11-08 21:29:55 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TDS200_h24_27',
+  webDir: 'dist'
+};
+
+export default config;
