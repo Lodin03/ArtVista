0cda8e625553ab20b576eb9c8c52e92ab3d5d1cd
Lodin03 <lamo007@student.kristiania.no>
2024-11-18 08:02:59 +0100

    .

--- src/views/UploadArtworkPage.vue
+++ src/views/UploadArtworkPage.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { toastController, IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonContent, IonList, IonItem, IonLabel, IonInput, IonButton, IonIcon, IonChip, IonImg, IonGrid, IonRow, IonCol } from '@ionic/vue';
+import { toastController, IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonContent, IonList, IonItem, IonLabel, IonInput, IonButton, IonIcon, IonChip, IonImg, IonGrid, IonRow, IonCol, IonSelect, IonSelectOption } from '@ionic/vue';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { Artwork } from '@/interfaces/Artwork';
-import { getFirestore, collection, setDoc, doc } from 'firebase/firestore';
+import { getFirestore, collection, setDoc, doc, query, where, getDocs } from 'firebase/firestore';
 import { add, trashOutline} from 'ionicons/icons';
 import { Camera, CameraResultType } from '@capacitor/camera';
 import { v4 as uuidv4 } from "uuid";
@@ -19,9 +19,10 @@ const newArtwork = ref<Artwork>({
   id: '',  
   title: '',
   abstract: '',
-  yearCreated: '',
+  yearCreated: 0,
   imageURL: '',
   hashtags: [], 
+  category: '',
   artist: {
     country: '',
     name: '',
@@ -31,6 +32,12 @@ const newArtwork = ref<Artwork>({
   },
   comments: []  
 });
+
+// Reactive variable to track if the artist already exists in the database
+const artistExists = ref(false);
+
+// Reactive variable to track if artist input fields should be shown
+const showArtistFields = ref(false);
 
 // Get a reference to the 'art_vista' collection in Firestore
 const artworkCollection = collection(getFirestore(), 'art_vista')
@@ -70,6 +77,51 @@ const postNewArtwork = async () => {
     }
 
     try {
+         // Checks if artwork already exists in db, by seeing if the name of the artwork exists already
+         const existingArtworkQuery = query(
+            artworkCollection,
+            where("title", "==", newArtwork.value.title)
+        );
+        const querySnapshot = await getDocs(existingArtworkQuery);
+
+        if (!querySnapshot.empty) {
+            alert("An artwork with this title already exists. Please choose a different title.");
+            setTimeout(() => {
+                location.reload()
+            }, 2000) // Refreshes the page after 2 seconds (2000ms)
+            return; 
+        }
+
+        // Check if artist the artist already exists in the db
+        const artistQuery = query(
+            artworkCollection,  
+            where("artist.name", "==", newArtwork.value.artist.name)
+        );
+
+        const artistSnapshot = await getDocs(artistQuery);
+
+        if (!artistSnapshot.empty) {
+            // Artist already exists, show alert and auto-fill artist details in the form
+            const existingArtist = artistSnapshot.docs[0].data().artist;
+
+            // Updating the form fields with the existing artist details
+            newArtwork.value.artist.country = existingArtist.country;
+            newArtwork.value.artist.yearBorn = existingArtist.yearBorn;
+            newArtwork.value.artist.yearDead = existingArtist.yearDead;
+            newArtwork.value.artist.about = existingArtist.about;
+
+            // Set artistExists to true to disable fields if artist exists
+            artistExists.value = true;
+
+            // Alert the user that the artist already exists and their details are being transferred
+            alert("This artist already exists in the database. Transferring existing information...");
+            
+        } else {
+            // Artist does not exist, show the artist input fields for the user to complete
+            showArtistFields.value = true;
+            artistExists.value = false;
+        }
+
         const generatedUUID = uuidv4() // Generates an unique ID
         const imageName = new Date().getTime() + ".jpg" // Generate a unique image name
         const storageRef = getStorage() 
@@ -160,6 +212,17 @@ const removeImagePreview = () => {
                 </ion-item>
 
                 <ion-item>
+                    <ion-label position="stacked">Category</ion-label>
+                    <ion-select v-model="newArtwork.category" interface="popover" placeholder="Select a category">
+                        <ion-select-option value="Portrait">Portrait</ion-select-option>
+                        <ion-select-option value="Landscape">Landscape</ion-select-option>
+                        <ion-select-option value="Architectural">Architectural</ion-select-option>
+                        <ion-select-option value="Abstract">Abstract</ion-select-option>
+                        <ion-select-option value="Religious">Religious</ion-select-option>
+                    </ion-select>
+                </ion-item>
+
+                <ion-item>
                     <ion-label position="stacked">Hashtags</ion-label>
                     <ion-input v-model="newHashtags" type="text"></ion-input>
 
@@ -173,62 +236,59 @@ const removeImagePreview = () => {
                 </ion-item>
 
                 <ion-grid>
-                <ion-row>
-                    <ion-col size="6">
-                        <ion-item>
-                            <ion-label position="stacked">Artist Name</ion-label>
-                            <ion-input v-model="newArtwork.artist.name" type="text"></ion-input>
-                        </ion-item>
-                    </ion-col>
-                    <ion-col size="6">
-                        <ion-item>
-                            <ion-label position="stacked">Artist Country</ion-label>
-                            <ion-input v-model="newArtwork.artist.country" type="text"></ion-input>
-                        </ion-item>
-                    </ion-col>
-                </ion-row>
+                    <ion-row>
+                        <ion-col size="6">
+                            <ion-item>
+                                <ion-label position="stacked">Artist Name</ion-label>
+                                <ion-input v-model="newArtwork.artist.name" type="text"></ion-input>
+                            </ion-item>
+                        </ion-col>
+                        <ion-col size="6">
+                            <ion-item>
+                                <ion-label position="stacked">Artist Country</ion-label>
+                                <ion-input v-model="newArtwork.artist.country" type="text"></ion-input>
+                            </ion-item>
+                        </ion-col>
+                    </ion-row>
 
-                <ion-row>
-                    <ion-col size="6">
-                        <ion-item>
-                            <ion-label position="stacked">Year Born</ion-label>
-                            <ion-input v-model="newArtwork.artist.yearBorn" type="number" min="0" max="2024"></ion-input>
-                        </ion-item>
-                    </ion-col>
-                    <ion-col size="6">
-                        <ion-item>
-                            <ion-label position="stacked">Year Dead</ion-label>
-                            <ion-input v-model="newArtwork.artist.yearDead" type="number" min="0" max="2024"></ion-input>
-                        </ion-item>
-                    </ion-col>
-                </ion-row>
+                    <ion-row>
+                        <ion-col size="6">
+                            <ion-item>
+                                <ion-label position="stacked">Year Born</ion-label>
+                                <ion-input v-model="newArtwork.artist.yearBorn" type="number" min="0" max="2024"></ion-input>
+                            </ion-item>
+                        </ion-col>
+                        <ion-col size="6">
+                            <ion-item>
+                                <ion-label position="stacked">Year Dead</ion-label>
+                                <ion-input v-model="newArtwork.artist.yearDead" type="number" min="0" max="2024"></ion-input>
+                            </ion-item>
+                        </ion-col>
+                    </ion-row>
 
-                <ion-row>
-                    <ion-col size="12">
-                        <ion-item>
-                            <ion-label position="stacked">Artist Bio</ion-label>
-                            <ion-input v-model="newArtwork.artist.about" type="text"></ion-input>
-                        </ion-item>
-                    </ion-col>
-                </ion-row>
-            </ion-grid>
-
-                <ion-button @click="postNewArtwork" color="primary" class="upload-btn">
-                    Upload artwork
-                </ion-button>
+                    <ion-row>
+                        <ion-col size="12">
+                            <ion-item>
+                                <ion-label position="stacked">Artist Bio</ion-label>
+                                <ion-input v-model="newArtwork.artist.about" type="text"></ion-input>
+                            </ion-item>
+                        </ion-col>
+                    </ion-row>
+                    <ion-button @click="postNewArtwork" color="primary" class="upload-btn">Upload artwork</ion-button>
+                </ion-grid> 
             </ion-list>
         </ion-content>
     </ion-page>
 </template>
 
 <style scoped>
-ion-input {
+ion-input, ion-select {
     --color: var(--ion-color-medium)
 }
 
 .image-picker {
     display: flex;
-    height: 12.5rem;
+    height: 9rem;
     margin: 1rem;
     border: 0.125rem var(--ion-color-medium) dashed;
     border-radius: 0.5rem;
